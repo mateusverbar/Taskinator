3a8d9f517d3414c9c175729e00465ad15b7d11e3
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-08-29 00:30:55 -0500

    updating-tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var pageContentEl = document.querySelector("#page-content");
+
 var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
@@ -90,7 +92,43 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 };
 
+var taskButtonHandler = function(event) {
+    var targetEl = event.target;
+
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    else if (targetEl.matches(".delete-btn")) {
+      // get the element's task id
+      var taskId = targetEl.getAttribute("data-task-id");
+      deleteTask(taskId);
+    }
+  };
+
+  var editTask = function(taskId) {
+
+      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+      var taskName = taskSelected.querySelector("h3.task-name").textContent;
+
+      var taskType = taskSelected.querySelector("span.task-type").textContent;
+        document.querySelector("input[name='task-name']").value = taskName;
+        document.querySelector("select[name='task-type']").value = taskType;
+        document.querySelector("#save-task").textContent = "Save Task";
+
+        formEl.setAttribute("data-task-id", taskId);
+
+  };
+
+  var deleteTask = function(taskId) {
+      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+      taskSelected.remove();
+  };
+
 formEl.addEventListener("submit",taskFormHandler);
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 //You can create DOM objects without editing the html directly - i.e. that is, you can do it dynamically from js. 
 
